f50d740c1888f9e5f8899fe084128c6acccc6a19
ecehn <eric.chen5@yahoo.com>
2024-05-02 12:14:44 -0600

    commit before meeting

--- board/workouts.py
+++ board/workouts.py
@@ -5,43 +5,40 @@ from board.database import get_db
 
 bp = Blueprint("workouts", __name__)
 
-@bp.route('/workouts', methods=['GET', 'POST'])
+@bp.route('/workouts', methods=['POST'])
 def add_workout():
-    if request.method == 'POST':
-        data = request.form
-        exercise = data.get('exercise')
-        reps = data.get('reps')
-        sets = data.get('sets')
+    data = request.json
+    exercise = data.get('exercise')
+    reps = data.get('reps')
+    sets = data.get('sets')
 
-        if not exercise or not reps or not sets:
-            return jsonify({'error': 'Incomplete data provided'}), 400
+    if not exercise or not reps or not sets:
+        return jsonify({'error': 'Incomplete data provided'}), 400
 
-        # Add the workout to the database (you can add this part later)
+    database.insert_workout(exercise, reps, sets)
 
-        # Redirect to a different page after successful submission
-        return redirect(url_for('workouts.view_workout'))
-    else:
-        # If GET request, render the add.html template
-        return render_template('workouts/add.html')
+    # Redirect to the view page for the newly added workout
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute('SELECT last_insert_rowid()')
+    workout_id = cursor.fetchone()[0]
+    return redirect(url_for('view_workout', id=workout_id))
     
     
 # @bp.route('/viewworkout')
 # def view_workout():
 #     return render_template('workouts/view.html')
 
-@bp.route('/viewworkout')
-def view_workout():
+@bp.route('/workouts/<int:id>')
+def view_workout(id):
     db = get_db()
     cursor = db.cursor()
-    cursor.execute('SELECT * FROM workouts ORDER BY id DESC LIMIT 1')
+    cursor.execute('SELECT * FROM workouts WHERE id = ?', (id,))
     workout = cursor.fetchone()
-
-    if workout is None:
-        return jsonify({'error': 'No workouts found'}), 404
-
-    return jsonify({
-        'id': workout['id'],
-        'exercise': workout['exercise'],
-        'reps': workout['reps'],
-        'sets': workout['sets']
-    })
+    if workout:
+        # Render data as HTML page
+        return render_template('workout.html', workout=workout)
+        # Or return data as JSON
+        # return jsonify(workout)
+    else:
+        return 'Workout not found', 404
